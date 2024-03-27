16d07dd939889b47695f7ca1f27e5933f4553f6c
cyche12 <141588141+cyche12@users.noreply.github.com>
2024-03-27 12:43:51 -0400

    Base Recipe Class added - Android Group Project
    Signed-off-by: cyche12 <141588141+cyche12@users.noreply.github.com>

---
+++ app/src/main/java/algonquin/cst2335/android_group_project/RecipeSearch.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.android_group_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
