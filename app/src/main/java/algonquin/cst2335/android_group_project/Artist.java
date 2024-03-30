8eb9a009951163d3b1745c10d73d08dcc8c00501
cyche12 <141588141+cyche12@users.noreply.github.com>
2024-03-30 15:28:01 -0400

    Adding Shilpi API
    Signed-off-by: cyche12 <141588141+cyche12@users.noreply.github.com>

---
+++ app/src/main/java/algonquin/cst2335/android_group_project/Artist.java
@@ -0,0 +1,24 @@
+package algonquin.cst2335.android_group_project;
+public class Artist {
+
+    private int id;
+    private String name;
+    private String pictureMedium;
+    private String tracklist;
+
+    // Constructor
+    public Artist(String name, String pictureMedium, String tracklist) {
+        this.id = id;
+        this.name = name;
+        this.pictureMedium = pictureMedium;
+        this.tracklist = tracklist;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getPictureMedium() { return pictureMedium; }
+    public String getTracklist() { return tracklist; }
+}
+
+
