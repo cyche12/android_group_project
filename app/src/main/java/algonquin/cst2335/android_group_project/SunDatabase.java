4a66297c129efd8ff4a19a0137dc0f7100a4c9b7
cyche12 <141588141+cyche12@users.noreply.github.com>
2024-03-31 18:44:26 -0400

    Current working version
    Signed-off-by: cyche12 <141588141+cyche12@users.noreply.github.com>

---
+++ app/src/main/java/algonquin/cst2335/android_group_project/SunDatabase.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.android_group_project;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Sunrise_Data.class}, version = 1, exportSchema = false)
+public abstract class SunDatabase extends RoomDatabase {
+    public abstract SunDAO sunriseDao();
+}
