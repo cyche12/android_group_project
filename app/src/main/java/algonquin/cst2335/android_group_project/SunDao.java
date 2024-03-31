fe4ffdfa162d78c3167093ddb63c98a1c9cdb769
cyche12 <141588141+cyche12@users.noreply.github.com>
2024-03-31 18:46:13 -0400

    Current working version
    Signed-off-by: cyche12 <141588141+cyche12@users.noreply.github.com>

---
+++ app/src/main/java/algonquin/cst2335/android_group_project/SunDao.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.android_group_project;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SunDao {
+    @Insert
+    void insert(Sunrise_Data sunriseData);
+
+    @Query("SELECT * FROM sunrise_data")
+    List<SunDatabase> getAllSunriseData();
+}
